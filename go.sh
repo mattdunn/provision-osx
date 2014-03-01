c637dea12fe5baf474038ddf4b7aa3dc6d4304d7
mattdunn <contact@mattonsoftware.com>
2014-03-01 22:37:30 +1100

    install ansible

--- go.sh
+++ go.sh
@@ -7,5 +7,8 @@ brew update && brew doctor
 echo Installing Homebrew-Cask...
 brew install phinze/cask/brew-cask
 
+echo Installing Ansible
+brew install ansible
+
 echo Manual things...
 echo  - Create and upload a new ssh key for GitHub
