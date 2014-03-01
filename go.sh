5afcd911c2d0618bd00334efbd40cd5620a0947e
mattdunn <contact@mattonsoftware.com>
2014-03-01 23:02:03 +1100

    cleanup previous homebrew installs and wait after brew doctor

--- go.sh
+++ go.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+if ! has 'brew'; then
+  echo "Uninstalling Homebrew..."
+  rm -rf /usr/local/Cellar /usr/local/.git && brew cleanup
+fi
+
 echo Installing Homebrew...
 ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
-brew update && brew doctor
+
+echo Updating Homebrew...
+brew update
+
+echo Checking Homebrew health...
+brew doctor
+read -p "Press any key to continue or ctrl-z to quit... "
 
 echo Installing Homebrew-Cask...
 brew install phinze/cask/brew-cask
