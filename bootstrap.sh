d6a31f4268f9a723b64233d60e67ec93a2d98fc3
Matt Dunn <contact@mattonsoftware.com>
2014-03-02 23:05:03 +1100

    create git repo locally and add remote instead of git clone

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 GIT_REPO_NAME=provision-osx
-GIT_REPO_LOCATION=https://github.com/mattdunn/$GIT_REPO.git
+GIT_REPO_LOCATION=https://github.com/mattdunn/$GIT_REPO
 GIT_REPO_CLONE_LOCATION=~/projects/$GIT_REPO
 
 has() {
@@ -19,7 +19,9 @@ cleanup(){
 clone_repo(){
   mkdir -p $GIT_REPO_CLONE_LOCATION
   cd $GIT_REPO_CLONE_LOCATION
-  git clone $GIT_REPO_LOCATION .
+  git init -q
+  git remote add origin $GIT_REPO_LOCATION
+  git fetch origin master:refs/remotes/origin/master -n
 }
 
 if has 'brew'; then
