57ea7512f1088a0aa5e4e4e182a97e4b8e1fff5d
Matt Dunn <contact@mattonsoftware.com>
2016-01-06 11:47:29 +1100

    update homebrew install location

--- provision.sh
+++ provision.sh
@@ -2,7 +2,7 @@
 
 echo "Installing Homebrew..."
 echo ""
-ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 echo "Updating Homebrew..."
 echo ""
